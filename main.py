26b530cd52c67581eccf7e7f93b77823a00d941e
Jonathan <114219464+Auca-XT@users.noreply.github.com>
2022-10-27 08:53:09 +0000

    add dice range (a, b)

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ for x in range(1):
   rolldice = rolldice + random.choices(dice1)[0]
 
 print('Dice roll out come:\n', rolldice)
+
+print('\n', "other dice", '\n')
+
+#dice with range a, b
+
+for a in range(1,10):
+ Worp = random.randint(1,6)
+print("Worp",a, "was:", Worp)
